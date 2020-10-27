124ae41961616b311f9b9e3606264a935b819084
MadCode1982 <528764698@qq.com>
2020-10-27 17:38:41 +0800

    桥接模式

---
+++ CodeDemo.DesignPattern/StructurePattern/BridgePattern/ExtendedAbstraction.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeDemo.DesignPattern.StructurePattern.BridgePattern
+{
+    class Abstraction
+    {
+        protected IImplementation _implementation;
+
+        public Abstraction(IImplementation implementation)
+        {
+            this._implementation = implementation;
+        }
+
+        public virtual string Operation()
+        {
+            return "Abstract: Base operation with:\n" +
+                _implementation.OperationImplementation();
+        }
+    }
+
+    // You can extend the Abstraction without changing the Implementation
+    // classes.
+    //您可以扩展抽象，而无需更改实现
+    //类。
+    class ExtendedAbstraction : Abstraction
+    {
+        public ExtendedAbstraction(IImplementation implementation) : base(implementation)
+        {
+        }
+
+        public override string Operation()
+        {
+            return "ExtendedAbstraction: Extended operation with:\n" +
+                base._implementation.OperationImplementation();
+        }
+    }
+
+    // The Implementation defines the interface for all implementation classes.
+    // It doesn't have to match the Abstraction's interface. In fact, the two
+    // interfaces can be entirely different. Typically the Implementation
+    // interface provides only primitive operations, while the Abstraction
+    // defines higher- level operations based on those primitives.
+    //实现定义所有实现类的接口。
+    //它不必匹配Abstraction的接口。 其实这两个
+    //接口可以完全不同。 通常实施
+    //接口仅提供原始操作，而Abstraction
+    //根据这些原语定义更高级别的操作。
+    public interface IImplementation
+    {
+        string OperationImplementation();
+    }
+
+    // Each Concrete Implementation corresponds to a specific platform and
+    // implements the Implementation interface using that platform's API.
+    //每个具体实现都对应一个特定平台，并且
+    //使用该平台的API实现Implementation接口。
+    class ConcreteImplementationA : IImplementation
+    {
+        public string OperationImplementation()
+        {
+            return "ConcreteImplementationA: The result in platform A.\n";
+        }
+    }
+
+    class ConcreteImplementationB : IImplementation
+    {
+        public string OperationImplementation()
+        {
+            return "ConcreteImplementationA: The result in platform B.\n";
+        }
+    }
+
+    class Client
+    {
+        // Except for the initialization phase, where an Abstraction object gets
+        // linked with a specific Implementation object, the client code should
+        // only depend on the Abstraction class. This way the client code can
+        // support any abstraction-implementation combination.
+        //除了初始化阶段外，在该阶段获取Abstraction对象
+        //与特定的Implementation对象链接后，客户端代码应
+        //仅取决于Abstraction类。 这样客户端代码可以
+        //支持任何抽象实现组合。
+        public void ClientCode(Abstraction abstraction)
+        {
+            Console.Write(abstraction.Operation());
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+
+            Abstraction abstraction;
+            // The client code should be able to work with any pre-configured
+            // abstraction-implementation combination.
+            //客户端代码应能够与任何预配置一起使用
+            //抽象实现组合。
+            abstraction = new Abstraction(new ConcreteImplementationA());
+            client.ClientCode(abstraction);
+
+            Console.WriteLine();
+
+            abstraction = new ExtendedAbstraction(new ConcreteImplementationB());
+            client.ClientCode(abstraction);
+        }
+    }
+}
